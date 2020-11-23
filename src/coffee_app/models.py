77eeb057e72ab6f10cdea544047dd053f8661cae
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-11-23 00:00:21 -0400

    added facebook link

--- src/coffee_app/models.py
+++ src/coffee_app/models.py
@@ -5,3 +5,4 @@ class CoffeeShop(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=360)
     image = models.ImageField(null=True)
+    website = models.CharField(max_length=200,null=True)
