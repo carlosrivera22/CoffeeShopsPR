8be9a4dbfa05b9351d4d1759e6b56f1ddbccbb1d
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-11-23 23:55:33 -0400

    created book model object

--- src/coffee_app/admin.py
+++ src/coffee_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CoffeeShop
+from .models import CoffeeShop, Book
 
 # Register your models here.
 admin.site.register(CoffeeShop)
+admin.site.register(Book)
--- src/coffee_app/models.py
+++ src/coffee_app/models.py
@@ -7,3 +7,11 @@ class CoffeeShop(models.Model):
     image = models.ImageField(null=True)
     facebook = models.CharField(max_length=200,null=True)
     address = models.CharField(max_length=200,null=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=360)
+    image = models.ImageField()
+    author = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
