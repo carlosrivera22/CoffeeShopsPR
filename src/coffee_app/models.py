e6dc13264963d9334a7c376c35801af383963382
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-11-23 23:34:32 -0400

    adding address to shop

--- src/coffee_app/models.py
+++ src/coffee_app/models.py
@@ -5,4 +5,5 @@ class CoffeeShop(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=360)
     image = models.ImageField(null=True)
-    website = models.CharField(max_length=200,null=True)
+    facebook = models.CharField(max_length=200,null=True)
+    address = models.CharField(max_length=200,null=True)
