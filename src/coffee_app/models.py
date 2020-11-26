6b953c24fc1676383fdd90ac875ca54dfd428338
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-11-26 10:23:25 -0400

    added lat lon fields

--- src/coffee_app/models.py
+++ src/coffee_app/models.py
@@ -7,6 +7,10 @@ class CoffeeShop(models.Model):
     image = models.ImageField(null=True)
     facebook = models.CharField(max_length=200,null=True)
     address = models.CharField(max_length=200,null=True)
+    latitude = models.FloatField(null=True,blank=True)
+    longitude = models.FloatField(null=True,blank=True)
+
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
