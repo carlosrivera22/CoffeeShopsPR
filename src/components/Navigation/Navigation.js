ba223567198b890d94d42a941b2c943fe90f37ac
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-12-16 16:40:40 -0400

    fixing navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import { Navbar, Nav} from 'react-bootstrap';
 const Navigation = () => {
     return ( 
         <Navbar bg="dark" variant="dark" className="mb-3" expand="sm">
-            <Navbar.Brand style={{height:"55px"}} href="/">Coffee Shops PR</Navbar.Brand>
+            <Navbar.Brand href="/">Coffee Shops PR</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
