e2b7b6df07c5824e5ae543ae79a3e8aa179a2d7a
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-12-16 11:00:14 -0400

    adding template

--- coffee_shops/settings.py
+++ coffee_shops/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'coffee_shops.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build')
+        'DIRS': [os.path.join(BASE_DIR, 'build'),
                  os.path.join(BASE_DIR, 'staticfiles')
         ],
         'APP_DIRS': True,
