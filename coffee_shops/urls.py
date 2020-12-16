b65cb6270ec6a5dd146c0010bd3ab56afd695628
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-12-16 10:35:35 -0400

    adding template

--- coffee_shops/urls.py
+++ coffee_shops/urls.py
@@ -19,7 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from .views import index
 urlpatterns = [
-    path(r'^/$',index,name='index'),
+    path(r'^$',index,name='index'),
+    path(r'^(?:.*)/?$',index),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('coffee_app.api.urls',namespace='api-coffee-shops'))
