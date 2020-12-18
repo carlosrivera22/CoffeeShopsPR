241e4be8cbd12ef7c4c78ad082429b6f847ef904
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-12-17 21:26:21 -0400

    login required api

--- coffee_app/api/urls.py
+++ coffee_app/api/urls.py
@@ -1,15 +1,15 @@
-# from .views import CoffeeShopViewSet, NewsletterViewSet, BookViewSet,CoffeeShopBooksViewSet
+from .views import CoffeeShopViewSet, NewsletterViewSet, BookViewSet,CoffeeShopBooksViewSet
 from rest_framework import routers
 from django.urls import path, include
 
 app_name = 'api-coffee-shops'
 router = routers.DefaultRouter()
-router.register(r'coffee_shops')
-router.register(r'coffee_shops/(?P<id>\d+)')
-router.register(r'newsletter')
-router.register(r'books')
-router.register(r'books/(?P<id>\d+)')
-router.register(r'coffee_shop/(?P<id>\d+)/books')
+router.register(r'coffee_shops', CoffeeShopViewSet)
+router.register(r'coffee_shops/(?P<id>\d+)', CoffeeShopViewSet)
+router.register(r'newsletter', NewsletterViewSet)
+router.register(r'books',BookViewSet)
+router.register(r'books/(?P<id>\d+)',BookViewSet)
+router.register(r'coffee_shop/(?P<id>\d+)/books',CoffeeShopBooksViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- coffee_app/api/views.py
+++ coffee_app/api/views.py
@@ -1,28 +1,33 @@
-# from rest_framework import viewsets
-# from .serializers import CoffeeShopSerializer, NewsletterSerializer, BookSerializer
-# from ..models import CoffeeShop, Newsletter,Book
-# from rest_framework.response import Response
-# class CoffeeShopViewSet(viewsets.ModelViewSet):
-#     queryset = CoffeeShop.objects.all()
-#     serializer_class = CoffeeShopSerializer
+from rest_framework import viewsets
+from .serializers import CoffeeShopSerializer, NewsletterSerializer, BookSerializer
+from ..models import CoffeeShop, Newsletter,Book
+from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
 
-# class NewsletterViewSet(viewsets.ModelViewSet):
-#     queryset = Newsletter.objects.all()
-#     serializer_class = NewsletterSerializer
+@login_required
+class CoffeeShopViewSet(viewsets.ModelViewSet):
+    queryset = CoffeeShop.objects.all()
+    serializer_class = CoffeeShopSerializer
 
-# class BookViewSet(viewsets.ModelViewSet):
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
+@login_required
+class NewsletterViewSet(viewsets.ModelViewSet):
+    queryset = Newsletter.objects.all()
+    serializer_class = NewsletterSerializer
+
+@login_required
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
     
-
-# class CoffeeShopBooksViewSet(viewsets.ModelViewSet):
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
-#     def get_queryset(self):
-#         shop_id = self.request.parser_context["kwargs"]["id"]
-#         shop = CoffeeShop.objects.filter(id=shop_id)
-#         books = Book.objects.filter(shop=shop[0])
-#         return books
+@login_required
+class CoffeeShopBooksViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    def get_queryset(self):
+        shop_id = self.request.parser_context["kwargs"]["id"]
+        shop = CoffeeShop.objects.filter(id=shop_id)
+        books = Book.objects.filter(shop=shop[0])
+        return books
                 
 
         
