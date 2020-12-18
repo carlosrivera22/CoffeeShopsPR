1da66d221963defbaba79062be535e69808f2317
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-12-17 22:01:14 -0400

    top spacing style

--- coffee_app/api/views.py
+++ coffee_app/api/views.py
@@ -2,26 +2,26 @@ from rest_framework import viewsets
 from .serializers import CoffeeShopSerializer, NewsletterSerializer, BookSerializer
 from ..models import CoffeeShop, Newsletter,Book
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 class CoffeeShopViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
     queryset = CoffeeShop.objects.all()
     serializer_class = CoffeeShopSerializer
 
 class NewsletterViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
     queryset = Newsletter.objects.all()
     serializer_class = NewsletterSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
 
 class CoffeeShopBooksViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     def get_queryset(self):
--- coffee_shops/settings.py
+++ coffee_shops/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'coffee_shops.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'public')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -157,7 +157,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 django_heroku.settings(locals()) 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
 STATICFILES_DIRS = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
