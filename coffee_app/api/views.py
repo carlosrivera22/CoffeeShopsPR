0eb63b0a11f00bb40395a1762aa7b0729e8f4fc0
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-12-17 22:10:50 -0400

    handling permissions

--- coffee_app/api/views.py
+++ coffee_app/api/views.py
@@ -2,26 +2,26 @@ from rest_framework import viewsets
 from .serializers import CoffeeShopSerializer, NewsletterSerializer, BookSerializer
 from ..models import CoffeeShop, Newsletter,Book
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class CoffeeShopViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = CoffeeShop.objects.all()
     serializer_class = CoffeeShopSerializer
 
 class NewsletterViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Newsletter.objects.all()
     serializer_class = NewsletterSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
 
 class CoffeeShopBooksViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     def get_queryset(self):
