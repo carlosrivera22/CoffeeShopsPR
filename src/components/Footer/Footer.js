5e75ddb557f0cf308580cd056013a6a30f23e4fa
Carlos Rivera <carlos.rivera@econsgroup.com>
2020-12-18 20:52:38 -0400

    details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return ( 
         <footer className='footer mt-auto py-3 bg-dark text-white'>
-            <div className='container'>&copy; 2020 Coffee Shops PR.</div>
+            <div className='container'>&copy; 2021  DE LA ISLA</div>
         </footer>
      );
 }
